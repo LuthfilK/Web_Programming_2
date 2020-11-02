d5325f08b12896fb27f918ffc389413eb5865121
febriaji <marcm3670@gmail.com>
2020-11-03 01:41:40 +0700

    WP2

---
+++ pertemuan1/perkenalan.php
@@ -0,0 +1,12 @@
+<?php
+class perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalan </h1>";
+        echo "Nama saya LUTHFIL KHAKIM<br>
+              Saya tinggal di Adiwerna, Tegal, Jawa Tengah, Indonesia <br>
+              Saya suka ngegame";
+
+    }
+}
